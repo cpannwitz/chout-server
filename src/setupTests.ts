6d12c981a37f0018b24e862f61cf0fc31bff93bc
Christian Pannwitz <ch.von.pa@googlemail.com>
2019-07-28 20:23:05 +0200

    Feature/fixing typeorm connections migrations (#5)
    * this was the line
    
    * Feature/add test entity change for migrations (#4)
    
    * added user entity column locked
    
    * fixed it!
    
    * changed typeorm config to be env agnostic
    
    * fixed migrations command for remote

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config'
 import getDatabase from './services/database'
 import getRedis from './services/redis'
 
 async function setupTests() {
   getRedis()
-  return await getDatabase().catch(error => console.error(error))
+  return await getDatabase('test').catch(error => console.error(error))
 }
 
 export default setupTests
