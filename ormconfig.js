f418a50eee1409a3ccd999895f0c0cb7b54bff1c
Christian Pannwitz <ch.von.pa@googlemail.com>
2019-07-28 22:23:19 +0200

    first try without persistingworkspace (#8)
    * first try without persistingworkspace
    
    * removed filtering
    
    * meh, stupid mistake
    
    * another try
    
    * and again
    
    * maybe this will work
    
    * well we got with sequential builds
    
    * ah cmon
    
    * oh yes
    
    * and yeah

--- ormconfig.js
+++ ormconfig.js
@@ -71,9 +71,9 @@ module.exports = [
   synchronize:true,
   dropSchema: true,
   cache: false,
-  entities: ['./src/modules/**/*.entity.ts'],
-  migrations: ['./src/migration/*.ts'],
-  subscribers: ['./src/subscriber/**/*'],
+  entities: ['./dist/modules/**/*.entity.js'] ,
+  migrations: ['./dist/migration/*.js'] ,
+  subscribers: ['./dist/subscriber/**/*'] ,
   cli: {
     entitiesDir: './src/modules',
     migrationsDir: './src/migration/',
@@ -109,105 +109,3 @@ module.exports = [
   }
 },
 ]
-
-//   {
-//   name: 'default',
-//   url: DATABASE_URL,
-//   host: DBHOST,
-//   port: DBPORT,
-//   database: DBDATABASE,
-//   username: DBUSER,
-//   password: DBPASSWORD,
-//   type: 'postgres',
-//   logging: 'all',
-//   synchronize: false,
-//   dropSchema: false,
-//   cache: true,
-//   entities: ['./dist/modules/**/*.entity.js'],
-//   migrations: ['./dist/migration/*.js'],
-//   subscribers: ['./dist/subscriber/**/*'],
-//   cli: {
-//     entitiesDir: './src/modules',
-//     migrationsDir: './src/migration/',
-//     subscribersDir: './src/subscriber'
-//   },
-//   extra: {
-//     ssl: false
-//   }
-// },
-//   {
-//   name: 'development',
-//   url: DATABASE_URL,
-//   host: DBHOST,
-//   port: DBPORT,
-//   database: DBDATABASE,
-//   username: DBUSER,
-//   password: DBPASSWORD,
-//   type: 'postgres',
-//   logging: 'all',
-//   synchronize: false,
-//   dropSchema: false,
-//   cache: true,
-//   entities: ['./src/modules/**/*.entity.ts'],
-//   migrations: ['./src/migration/*.js'],
-//   subscribers: ['./src/subscriber/**/*'],
-//   cli: {
-//     entitiesDir: './src/modules',
-//     migrationsDir: './src/migration/',
-//     subscribersDir: './src/subscriber'
-//   },
-//   extra: {
-//     ssl: false
-//   }
-// },
-//   {
-//   name: 'production',
-//   url: DATABASE_URL,
-//   host: DATABASE_URL ? undefined : DBHOST,
-//   port: DATABASE_URL ? undefined : DBPORT,
-//   database: DATABASE_URL ? undefined : DBDATABASE,
-//   username: DATABASE_URL ? undefined : DBUSER,
-//   password: DATABASE_URL ? undefined : DBPASSWORD,
-//   type: 'postgres',
-//   logging: false,
-//   synchronize: false,
-//   dropSchema: false,
-//   cache: true,
-//   entities: ['./dist/modules/**/*.entity.js'],
-//   migrations: ['./dist/migration/*.js'],
-//   subscribers: ['./dist/subscriber/**/*'],
-//   cli: {
-//     entitiesDir: './src/modules',
-//     migrationsDir: './src/migration/',
-//     subscribersDir: './src/subscriber'
-//   },
-//   extra: {
-//     ssl: true
-//   }
-// },
-//   {
-//   name: 'test',
-//   url: DATABASE_URL,
-//   host: DBHOST,
-//   port: DBPORT,
-//   database: DBDATABASE,
-//   username: DBUSER,
-//   password: DBPASSWORD,
-//   type: 'postgres',
-//   logging: false,
-//   synchronize: true,
-//   dropSchema: true,
-//   cache: false,
-//   entities: ['./dist/modules/**/*.entity.js'],
-//   migrations: ['./dist/migration/*.js'],
-//   subscribers: ['./dist/subscriber/**/*'],
-//   cli: {
-//     entitiesDir: './src/modules',
-//     migrationsDir: './src/migration/',
-//     subscribersDir: './src/subscriber'
-//   },
-//   extra: {
-//     ssl: false
-//   }
-// },
-// ]
