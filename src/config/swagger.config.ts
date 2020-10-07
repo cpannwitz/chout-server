24ed733fc8d5f79a31c9300946d211da415cb2bd
chris <ch.von.pa@gmail.com>
2020-10-07 21:24:13 +0200

    added auth for swagger documentation

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,12 +1,30 @@
 import { INestApplication } from '@nestjs/common'
-import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import basicAuth from 'express-basic-auth'
+import { version } from '../../package.json'
+
+const DEFAULT_DOCS_PATH = '/api'
+const DEFAULT_DOCS_USER = process.env.DOCS_USER || 'chout'
+const DEFAULT_DOCS_PASS = process.env.DOCS_PASS || 'chout'
 
 export default (app: INestApplication) => {
+  // enable basic authentication for OpenAPI documentation with credentials
+  app.use(
+    DEFAULT_DOCS_PATH,
+    basicAuth({
+      challenge: true,
+      users: { [DEFAULT_DOCS_USER]: DEFAULT_DOCS_PASS }
+    })
+  )
+
+  // configure the OpenAPI documentation
   const options = new DocumentBuilder()
     .setTitle('Chout - Events')
     .setDescription('Chout API Backend description')
-    .setVersion(process.env.API_VERSION || 'v1')
+    .setVersion(version || 'v1')
     .build()
+
+  // initialize the OpenAPI documentation
   const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup(DEFAULT_DOCS_PATH, app, document)
 }
